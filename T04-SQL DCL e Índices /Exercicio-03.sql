77536bfec1fb8c0afac45047fdad09617fad1e95
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-10-20 20:48:43 -0300

    Atualizando questao 3

--- T04-SQL DCL e Índices /Exercicio-03.sql
+++ T04-SQL DCL e Índices /Exercicio-03.sql
@@ -1,2 +1,5 @@
-/* 3 - Crie um usuário chamado empresa_admim_local para o banco de dados EmpresaDB. Este usuário deve ter todos os privilégios mas sem acesso remoto.
-*/
+/* 3 - Crie um usuário chamado empresa_admim_local para o banco de dados EmpresaDB. Este usuário deve ter todos os privilégios mas sem acesso remoto.*/
+
+CREATE USER 'empresa_admim_local'@localhost' IDENTIFIED BY '13579';
+GRANT ALL PRIVILEGES ON 'EmpresaDB'.* TO 'empresa_admim_local'@'localhost';
+FLUSH PRIVILEGES;
