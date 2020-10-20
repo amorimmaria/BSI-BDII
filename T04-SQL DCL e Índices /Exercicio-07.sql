ff39f04dd9b77806e5c9be478e866fbcd5a44f3a
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-10-20 20:34:22 -0300

    Adicionando questao 7

---
+++ T04-SQL DCL e Índices /Exercicio-07.sql
@@ -0,0 +1,4 @@
+/* 7 - Crie um índice para o atributo sigla da tabela departamento e um índice para o atributo situação da tabela atividade.*/
+
+CREATE INDEX Ind_sigla ON Departamento(sigla);
+CREATE INDEX Ind_situacao ON Atividade(situacao);
