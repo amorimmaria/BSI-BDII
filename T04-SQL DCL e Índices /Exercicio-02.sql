fce7b7fd854c3dd4b0be4ccbb0d4f32ed130e95c
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-10-20 20:28:33 -0300

    Adicionando questao 2

---
+++ T04-SQL DCL e Índices /Exercicio-02.sql
@@ -0,0 +1,3 @@
+/* 2 - Explique as diferentes opções de codificação de caracteres e de collations.*/
+-- A codificação de caracteres(charset) define o conjunto de caracteres que você utilizará para armazenar dados em forma de texto, e existe charset para vários idiomas.
+-- As collations definem as regras de comparação entre os caracteres do charset, se ignoram ou não letras minúsculas, maiúsculas, ou acentuação.
