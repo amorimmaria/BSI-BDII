62ffc6b2e8fa953520015896abe57c7053c29a27
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-10-20 20:24:41 -0300

    Adicionando questao 1

---
+++ T04-SQL DCL e Índices /Exercicio-01.sql
@@ -0,0 +1,2 @@
+/* 1 - Faça o comando de Criação do Banco de Dados acima informando a codificação de caracteres e o collate.*/
+CREATE DATABASE `EmpresaDB` CHARACTER SET utf8 COLLATE utf8_general_ci;
