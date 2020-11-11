ff7d76718e8fa08227299c2b5fd19740c7c56110
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-11-10 22:59:34 -0300

    Adicionando questao 12 da lista 05

---
+++ T05 - Lista I de Exercícios Funções e Procedimentos   /Exercicio-12.sql
@@ -0,0 +1,5 @@
+/* 12 - Crie um usuário chamado empresa_gerente para o banco de dados EmpresaDB. Este usuário deve ter privilégios apenas de fazer consultas em todas as tabelas. Ele pode inserir e atualizar as tabelas Equipe, Membro, Atividade e AtividadeProjeto.*/
+
+CREATE USER 'empresa_gerente'@'localhost' IDENTIFIED BY 'bsibd123';
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO empresa_gerente'@'localhost' IDENTIFIED BY 'bsibd123';
+GRANT INSERT, UPDATE ON (Equipe,Membro,Atividade,AtividadeProjeto) TO empresa_gerente'@'EmpresaDB' IDENTIFIED BY '54321';
