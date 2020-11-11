e44c3f0832d8ad3f83cbe35b89353f0e3b7b426d
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-11-10 23:00:25 -0300

    Adicionando questao 14 da lista 05

---
+++ T05 - Lista I de Exercícios Funções e Procedimentos   /Exercicio-14.sql
@@ -0,0 +1,4 @@
+/* 14 - Explique o funcionamento de um índice e como ele é implementado no SGBD.*/
+
+-- O índice ele funciona como um identificador para as informações que estão armazenadas nos bancos de dados, para que se torne mais fácil encontrar  registros diretamente pelo índice e de forma mais rápida.
+-- No SGBD são implementados de tal forma que os registros são armazenados em páginas de dados, ou seja, no que conhecemos como pilha, onde contém uma coleção de páginas de dados nas tabelas.
